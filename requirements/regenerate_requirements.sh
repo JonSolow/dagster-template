44bd9964fcee5fc715f63073bcd1109dea108ce2
Jon Solow <jssolow+gh@gmail.com>
2023-07-15 08:05:32 -0400

    Use resolver=backtracking

--- requirements/regenerate_requirements.sh
+++ requirements/regenerate_requirements.sh
@@ -6,11 +6,13 @@ set -ex
 ADDITIONAL_ARGS=$@
 
 pip-compile \
+    --resolver=backtracking \
     -o requirements.txt \
     $ADDITIONAL_ARGS \
     pyproject.toml
     
 pip-compile \
+    --resolver=backtracking \
     --extra=dev \
     -o dev-requirements.txt \
     $ADDITIONAL_ARGS \
