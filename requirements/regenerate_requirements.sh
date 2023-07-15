e8e2382ae8755a7b0c3b258c24aabbbe2f459a6d
Jon Solow <jssolow+gh@gmail.com>
2023-07-15 08:04:34 -0400

    Add additional arg option to script

--- requirements/regenerate_requirements.sh
+++ requirements/regenerate_requirements.sh
@@ -3,13 +3,15 @@
 
 set -ex
 
+ADDITIONAL_ARGS=$@
+
 pip-compile \
     -o requirements.txt \
+    $ADDITIONAL_ARGS \
     pyproject.toml
     
 pip-compile \
     --extra=dev \
     -o dev-requirements.txt \
-    pyproject.toml"
-
-
+    $ADDITIONAL_ARGS \
+    pyproject.toml
