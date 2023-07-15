02089152cc07528df05e4487fa42363caa6b3198
Jon Solow <jssolow+gh@gmail.com>
2023-07-15 08:10:36 -0400

    Revert "Use resolver=backtracking"
    This reverts commit 44bd9964fcee5fc715f63073bcd1109dea108ce2.

--- requirements/regenerate_requirements.sh
+++ requirements/regenerate_requirements.sh
@@ -6,13 +6,11 @@ set -ex
 ADDITIONAL_ARGS=$@
 
 pip-compile \
-    --resolver=backtracking \
     -o requirements.txt \
     $ADDITIONAL_ARGS \
     pyproject.toml
     
 pip-compile \
-    --resolver=backtracking \
     --extra=dev \
     -o dev-requirements.txt \
     $ADDITIONAL_ARGS \
